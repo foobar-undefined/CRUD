77a921356f7be0ac975d6819cd7158ead5048d88
foobar-undefined <mxfb1997@gmail.com>
2023-04-23 15:54:07 -0400

    setting up the environment & database on MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Load express
 const express = require('express');
+const logger = require('morgan');
 
 // create our express app
 const app = express();
@@ -7,13 +8,17 @@ const app = express();
 //app settings (app.set)
 app.set('view engine', 'ejs');
 
+//expose environment variables
+require('dotenv').config();
+// require an execute database config code
+require('./config/database');
+
 //mount middleware (app.use)
 app.use(logger('dev'));
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));     // this creates req.body from an HTML form submission
 
 //mount routes
-
 
 //Listen on port 3000 for HTTP request from clients
 app.listen(3000, () => {
