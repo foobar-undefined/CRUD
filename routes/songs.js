08c90ff981746087beefe35534cb59a28e2f2113
foobar-undefined <mxfb1997@gmail.com>
2023-04-25 19:00:48 -0400

    wip: delete functionality & ensure log in

--- routes/songs.js
+++ routes/songs.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const songController = require("../controllers/songs");
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 router.get("/", songController.index);
 
-router.get("/new", songController.new);
+router.get("/new", ensureLoggedIn, songController.new);
 
-router.post("/", songController.create);
+router.post("/", ensureLoggedIn, songController.create);
 
 router.get("/:id", songController.show);
 
